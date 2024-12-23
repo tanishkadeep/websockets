3f93f093922d6192118b23aeffef44e5f432a6ba
Tanishka <tanishkadeep09@gmail.com>
2024-12-23 08:44:25 +0530

    setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello world')
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", function connection(ws) {
+  console.log("user connected");
+  ws.send("something");
+
+  setInterval(() => {
+    ws.send(Math.random());
+  }, 500);
+});
