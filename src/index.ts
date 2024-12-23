f065dc1d1cf4a1575ddce1747bf43aef2793f2f7
Tanishka Deep <115184621+tanishkadeep@users.noreply.github.com>
2024-12-23 09:13:13 +0530

    echo app be

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,10 @@ const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", function connection(ws) {
   console.log("user connected");
-  //server to client
-  ws.send("something");
 
-  //client to server
   ws.on("message", function incoming(data) {
-    console.log(data.toString());
+    if (data.toString() === "ping") {
+      ws.send("pong");
+    }
   });
 });
