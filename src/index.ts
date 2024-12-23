ce76fb29e7054f8661f81d1ed2a08c6578b4d496
Tanishka <tanishkadeep09@gmail.com>
2024-12-23 09:07:29 +0530

    basics

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,11 @@ const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", function connection(ws) {
   console.log("user connected");
+  //server to client
   ws.send("something");
 
-  setInterval(() => {
-    ws.send(Math.random());
-  }, 500);
+  //client to server
+  ws.on("message", function incoming(data) {
+    console.log(data.toString());
+  });
 });
